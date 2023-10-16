e6d0e0b9ce0693df5fd4fc0bc166b757953a8613
Doreen <doreenmarie_cee@hotmail.com>
2023-10-16 23:36:56 +0200

    completing all iterations

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
+
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 const app = express();
 
@@ -28,6 +30,8 @@ const spotifyApi = new SpotifyWebApi({
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
+
+
 // Our routes go here:
 
 app.get('/', (req, res) => {
@@ -42,18 +46,61 @@ app.get('/artist-search', (req, res) => {
 
     spotifyApi.searchArtists(artistName)
     .then (data => {
+        // console.log('Received the following data from API:', data.body)
         const artists = data.body.artists.items;
+        console.log('Received artists:', artists);
 
+        res.render('artist-search-results' , {
+            title: 'Search Results',
+            artistName,
+            artists,
+        });
     })
+    .catch(err => {
+        console.log('Error while searching artists occured:', err);
+        res.status(500).send('An error occured while searching for artists');
+    });
+
+});
 
 
-})
+app.get('/albums/:artistId', (req, res, next) => {
+    const artistId = req.params.artistId;
+
+    spotifyApi.getArtistAlbums(artistId)
+    .then(data => {
+        const albums = data.body.items;
+
+        res.render('albums', {
+            title: 'Albums',
+            artistName: req.query.artistName, albums,
+        });
+    })
+    .catch(err => {
+        console.log('Error while searching artists occured:', err);
+        res.status(500).send('An error occured while searching for artists');
+    });
+
+});
 
 
+app.get('/album-tracks/: albumId' , (req, res, next) => {
+    spotifyApi.getAlbumTracks(albumId)
+    .then(data => {
+        const tracks = data.body.items;
 
+        res.render('album-tracks' , {
+            title: 'Album Tracks',
+            albumId,
+            tracks,
+        });
+    })
+    .catch(err => {
+        console.log('Error while searching artists occured:', err);
+        res.status(500).send('An error occured while searching for artists');
+    });
 
-
-
+});
 
 
 
